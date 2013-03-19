15cc8fd56b38ae7c34f9e4c4a7b7ba6a831459ea
Stas <stanislav.belkov@gmail.com>
2013-03-19 18:06:27 +1100

    Playlist player now working with track search results.

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -6,7 +6,7 @@ class TracksController < ApplicationController
 
   def index
     client = Soundcloud.new(:client_id => "f837b00cd8d79383183d405b33a8a25d")
-    @tracks = client.get('/tracks', :genres => params[:search], :limit => 10, :order => 'hotness')
+    @tracks = client.get('/tracks', :genres => params[:search], :limit => 20, :order => 'hotness')
     @track = params[:search]
   end
 
